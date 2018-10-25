ec0be027d22ca6369a906fc27260c7eeea68505d
wenweiliang <wenwiliang@gmail.com>
2018-10-25 20:19:56 +0000

    path2wxsvcf

--- google_api/generate_command.filtering.sh
+++ google_api/generate_command.filtering.sh
@@ -9,11 +9,11 @@
 while read lines; do
 	id=$(echo $lines | awk -F " " '{print $3}')
 	passvalue=$2
-	ref=$(echo $lines | awk -F " " '{if ($8=="HG19_Broad_variant") print "gs://dinglab/reference/Homo_sapiens_assembly19.fasta"; else print "gs://dinglab/reference/GRCh37-lite.fa"}')
+	ref=$(echo $lines | awk -F " " '{if ($8=="HG19_Broad_variant") print "gs://ding_lab_reference/Homo_sapiens_assembly19.fasta"; else print "gs://ding_lab_reference/GRCh37-lite.fa"}')
 	reffile=$(echo $lines | awk -F " " '{if ($8=="HG19_Broad_variant") print "Homo_sapiens_assembly19.fasta"; else print "GRCh37-lite.fa"}')
 	yaml="./germline_variant_snakemake/google_api/vcf_filtering.yaml"
-	wxs_path=$(echo $lines | awk -F " " '{print $23}')
+	wxs_path="gs://wliang/germline_snakemake/output/${passvalue}/${id}/prefilter.snp_indel.vcf.gz"
 	wgs_path="gs://wliang/germline_snakemake/output/${passvalue}/${id}/${id}.${passvalue}.merged.vcf"
 
-	gcloud alpha genomics pipelines run --pipeline-file ${yaml} --inputs fafile=${ref},faifile=${ref}.fai,ref=${reffile},id=${id},passvalue=${passvalue},wxsvcf=${wxs_path},wgsvcf=${wgs_path} --outputs outputPath=gs://wliang/germline_snakemake/output/${passvalue}/ --logging gs://gs://wliang/germline_snakemake/logging/compare/ --preemptible
+	gcloud alpha genomics pipelines run --pipeline-file ${yaml} --inputs fafile=${ref},faifile=${ref}.fai,ref=${reffile},id=${id},passvalue=${passvalue},wxsvcf=${wxs_path},wgsvcf=${wgs_path} --outputs outputPath=gs://wliang/germline_snakemake/output/${passvalue}/ --logging gs://wliang/germline_snakemake/logging/compare/ --preemptible
 done < $1
