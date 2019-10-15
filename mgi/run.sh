33e7a79929e943f78030d639865e82d93f25c6f3
Fernanda Martins Rodrigues <fernanda@wustl.edu>
2019-10-15 15:00:12 -0500

    add log directories and decompress interval files

--- mgi/run.sh
+++ mgi/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 echoerr() { printf "%s\n" "$*" >&2; }
 
 # Load the conda environment
@@ -7,11 +8,19 @@ if [ -z "$CONDA_DEFAULT_ENV" ]; then
     source activate /gscmnt/gc3024/dinglab/medseq/Noncoding_eQTL/tool/miniconda3/envs/snakemake
 fi
 
+# Decompress necessary files
+gunzip ../Files/IntervalFiles/b37/GRCh37.ensGene.coding.exon.interval_list.gz
+gunzip ../Files/IntervalFiles/b38/GRCh38.ensGene.coding.exon.interval_list.gz
+
 # Run snakemake via bsub
 echoerr "Launch the LSF/bsub master job to run snakemake ..."
+
+mkdir -p logs/snakemake_logs
+mkdir -p logs/lsf_logs
+
 bsub -a "docker(lbwang/dailybox)" -q research-hpc -N -o snakemake_job.log \
     env SHELL="/bin/bash" snakemake --rerun-incomplete --nolock \
     --jobs 100 \
-    --cluster "./bsub_submitter.py {dependencies} lsf_logs" \
+    --cluster "./bsub_submitter.py {dependencies} logs/lsf_logs" \
     --cluster-config bsub_config.json \
     -p all_tools
